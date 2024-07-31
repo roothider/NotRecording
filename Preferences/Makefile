ab4057eda6a68d91ae06c21f3a132ab17072fbc4
roothider <158083651+roothider@users.noreply.github.com>
2024-07-31 16:37:09 +0800

    update for roothide

--- Preferences/Makefile
+++ Preferences/Makefile
@@ -1,4 +1,6 @@
-ifeq ($(THEOS_PACKAGE_SCHEME),rootless)
+ifeq ($(THEOS_PACKAGE_SCHEME),roothide)
+TARGET := iphone:clang:16.2:15.0
+else ifeq ($(THEOS_PACKAGE_SCHEME),rootless)
 TARGET := iphone:clang:16.2:15.0
 else
 TARGET := iphone:clang:14.5:8.0
--- Shared.h
+++ Shared.h
@@ -1,4 +1,4 @@
-#import <rootless.h>
-#define NOT_RECORDING_PREFS_PATH ROOT_PATH_NS(@"/var/mobile/Library/Preferences/com.opa334.notrecordingprefs.plist")
+#import <roothide.h>
+#define NOT_RECORDING_PREFS_PATH jbroot(@"/var/mobile/Library/Preferences/com.opa334.notrecordingprefs.plist")
 
 extern void migratePreferencesIfNeeded(void);
